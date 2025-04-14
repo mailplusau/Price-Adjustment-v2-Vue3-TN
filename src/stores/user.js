ddaa9324212d372d83ad9adf11aa88a4f470e892
Tim Nguyen <tim.nguyen@mailplus.com.au>
2025-04-14 11:08:03 +1000

    Add more user roles to admin bucket

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,9 +1,11 @@
 import { defineStore } from 'pinia';
 import http from '@/utils/http.mjs';
+import { useMainStore } from "@/stores/main";
 
 const state = {
     id: 1732844,
-    role: null,
+    role: 1032,
+    name: 'Me',
 
     salesRep: {
         id: -1,
@@ -12,7 +14,7 @@ const state = {
 };
 
 const getters = {
-    isAdmin : state => [3, 1032].includes(state.role),
+    isAdmin : state => [3, 1032, 1001, 1022].includes(state.role), // Mail Plus Finance Role (1001), Mail Plus Finance Manager (1022)
     isFranchisee : state => state.role === 1000,
     notAdminOrFranchisee : state => ![3, 1000, 1032].includes(state.role),
     isMe : state => state.id === 1732844,
@@ -20,7 +22,16 @@ const getters = {
 
 const actions = {
     async init() {
-        let {role, id, salesRep} = await http.get('getCurrentUserDetails');
+        if (!top.location.href.includes('app.netsuite')) return;
+
+        if (useMainStore().testMode) {
+            this.role = 1000;
+            this.id = 1669342; // Brisbane
+            this.name = 'Adelaide CBD';
+            return;
+        }
+
+        let {role, id, name, salesRep} = await http.get('getCurrentUserDetails');
 
         this.role = parseInt(role);
         this.id = parseInt(id);
